3a2768f47b704dc039ea1e170c37c8941661c56b
Issac Nicholls <isaac.nicholls1@gmail.com>
2025-01-14 09:35:11 +0000

    heroku startup

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n++891r#5)pz=!7n*0$-eskkbjj5*&r=%=xe3a65_*^*z9bn(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
